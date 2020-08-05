39e1a59595e8ed752e2fece0c7d7b4308919db72
Maurizio Pireddu <pireddu.maurizio@gmail.com>
2020-08-05 22:24:24 +0100

    feat: content

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Content from '../src/Content';
 import SideHero from '../src/SideHero/SideHero';
 
 const Index = () => (
@@ -9,7 +10,7 @@ const Index = () => (
     </Head>
     <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
       <SideHero />
-      <div className="">2</div>
+      <Content />
     </div>
   </>
 );
---
+++ src/Content.js
@@ -0,0 +1,18 @@
+const Content = () => (
+  <div className="flex justify-center items-center">
+    <div className="p-12">
+      <h1 className="text-4xl font-semibold mb-6">Hi, I'm Alessandra</h1>
+      <p className="mb-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        eleifend quis lectus sed fermentum. Praesent dignissim eleifend est ac
+        mollis. Aliquam mattis, felis eget ultrices iaculis, sem nibh tincidunt
+        lacus, eu malesuada nisl ex id sem. Sed eget sagittis urna. Pellentesque
+        habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Quisque viverra pulvinar justo, sit amet sagittis dui fermentum
+        vitae. Vestibulum in tellus metus.
+      </p>
+      <p className="text-xl font-semibold italic">Something catchy</p>
+    </div>
+  </div>
+);
+export default Content;
