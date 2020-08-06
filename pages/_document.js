9bbef8f0c9cca910628cdf1b6ae3dd898658c06c
Maurizio Pireddu <pireddu.maurizio@gmail.com>
2020-08-06 22:02:49 +0100

    chore: add html lang

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class CustomDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
