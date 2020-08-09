9d1ec0115e3bcd21e4d92979bd3a21057374733e
Maurizio Pireddu <pireddu.maurizio@gmail.com>
2020-08-09 09:40:33 +0100

    chore: add font weight 900

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
             rel="stylesheet"
           />
           <meta
